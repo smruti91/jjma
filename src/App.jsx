806ec00f02b674a83c39969577402e732f69e8cf
smruti1991 <smruti.ranjan.leepu@gmail.com>
2023-05-22 16:09:58 +0530

    expertise complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Experties from "./components/Experties/Experties";
 import Hero from "./components/Hero/Hero";
 import Header from "./components/header/header";
 import css from './styles/app.module.scss';
@@ -7,6 +8,7 @@ const App = () => {
   return <div className={`bg-primary ${css.container}`}>
 <Header/>
 <Hero />
+<Experties />
   </div>
 };
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,32 +1,50 @@
 import React from 'react'
 import css from './Hero.module.scss'
+import { motion } from 'framer-motion'
+import { fadeIn, slideIn, staggerContainer } from '../../utils/motion'
 
 
 const Hero = () => {
   return (
     <div className={`paddings ${css.wrapper}`}>
-        <div className={`innerWidth ${css.container}`}>
+        <motion.div 
+         variants={staggerContainer}
+         initial="hidden"
+         whileInView="show"
+         viewport={{once:false,amount:0.25}}
+        className={`innerWidth ${css.container}`}>
             {/* Upper case element */}
             <div className={css.upperElement}>
-            <span className='primaryText'>
+            <motion.span
+            variants={fadeIn("right","tween",0.2,1)} 
+
+             className='primaryText'>
                     Hay there,<br /> I am Smruti
-                </span>
-                <span className='secondaryText'>
+                </motion.span>
+                <motion.span 
+                variants={fadeIn("left","tween",0.4,1)} 
+                className='secondaryText'>
                     I design butifull simple <br/>
                     I love What i do
-                </span>
+                </motion.span>
             </div>
              {/* person image */}
-            <div className={css.person}>
-                <img style={{width:'70%'}} src="person2.png" alt="person" />
-            </div>
+            <motion.div 
+            variants={fadeIn("up","tween",0.5,1)} 
+            className={css.person}>
+                <motion.img 
+                variants={slideIn("up","tween",0.5,1.3)} 
+                style={{width:'70%'}} src="person2.png" alt="person" />
+            </motion.div>
              {/* email */}
              <div className={css.email}>
                 <a href="mailto:sssmruti08@gmail.com">sssmruti08@gmail.com</a>
              </div>
              {/* Lower case element */}
             <div className={css.lowerElement}>
-               <div className={css.experience}>
+               <motion.div 
+               variants={fadeIn("right","tween",0.3,1)} 
+               className={css.experience}>
                    <div className="primaryText">10</div>
                    <div className="secondaryText">
                       <div>
@@ -36,14 +54,16 @@ const Hero = () => {
                         Experience
                       </div>
                    </div>
-               </div>
-               <div className={css.certificate}>
+               </motion.div>
+               <motion.div 
+               variants={fadeIn("left","tween",0.5,1)} 
+               className={css.certificate}>
                 <img src="./certificate.png" alt="certificate" />
                 <span>CERTIFIED PROFATIONAL</span>
                 <span>UI/UX DESIGNER</span>
-               </div>
+               </motion.div>
             </div>
-        </div>
+        </motion.div>
     </div>
   )
 }
